6ae985021b249fd9367a5c1d17ba59562a8f39d8
Glebokie <savostyk777@gmail.com>
2023-01-12 20:47:05 +0300

    init: complete reverse int task in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let numToStr = String(n);
+    let i = 0;
+    let result = '';
+    for (i = 0; i < numToStr.length; i++) {
+    result =`${numToStr[i]}${result}`;
+    } return +result;
 }
